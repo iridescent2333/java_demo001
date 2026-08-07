e95eb0b463f1ebc529c06445f4fb1743afd14d0f
iridescent2333 <qq2529777035@gmail.com>
2026-08-07 22:00:03 +0800

    完成启用禁用员工账号功能代码

--- backend/sky_main/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ backend/sky_main/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -112,4 +112,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return new PageResult(total,records);
     }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrstop(Integer status,Long id){
+        // update employee set status = ? where id = ?
+
+        /*Employee employee = new Employee();
+        employee.setStatus(status);
+        emplouee.setId(id);*/
+
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+
+        employeeMapper.update(employee);
+    }
 }
