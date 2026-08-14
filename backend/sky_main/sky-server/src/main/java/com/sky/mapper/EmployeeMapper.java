3de5346d6689c57499564bd6a3b060a29ccd4fb2
iridescent2333 <qq2529777035@gmail.com>
2026-08-15 00:08:11 +0800

    完成公共字段自动填充12代码开发

--- backend/sky_main/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ backend/sky_main/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -44,6 +44,12 @@ public interface EmployeeMapper {
 @AutoFill(value = OperationType.UPDATE)
 void update(Employee employee);
 
-@Select("select * from employee where id = #{id}")
+    /**
+     *
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
     Employee getById(Long id);
 }
